bad232729437d1e91907907867793a6c30e452c8
kweku76 <daffran@gmail.com>
2020-04-02 23:33:35 +1000

    Updates

---
+++ precache-manifest.a4ec925c9d57216881597f4df8b9be0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07aaf3ec3ff65ac10b2f3af0ceb1162",
+    "url": "/react04/index.html"
+  },
+  {
+    "revision": "861141d7ddf5ec212f46",
+    "url": "/react04/static/css/main.67bbe2f5.chunk.css"
+  },
+  {
+    "revision": "640758405bb67c443987",
+    "url": "/react04/static/js/2.80ea7169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react04/static/js/2.80ea7169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861141d7ddf5ec212f46",
+    "url": "/react04/static/js/main.7bdf881b.chunk.js"
+  },
+  {
+    "revision": "b377690ebdc099fe7f8a",
+    "url": "/react04/static/js/runtime-main.db2dfa4e.js"
+  }
+]);
